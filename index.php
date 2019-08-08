27b95ec565098a5531a0b7b4559b23dc532e56fb
marcosmateom <42660787+marcosmateom@users.noreply.github.com>
2019-08-08 15:49:49 -0600

    formulario de login creado y login_controller creado pero en blanco

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login_controller.php">
+        <div class="container">
+            <label for="usuario"><b>Usuario</b></label>
+            <br>
+            <input type="text" placeholder="Ingrese el usuario" name="usuario" required>
+            <br>
 
+            <label for="pass"><b>Contraseña</b></label>
+            <br>
+            <input type="password" placeholder="Ingrese su contraseña" name="pass" required>
+            <br>
 
-
-?>
+            <button type="submit">Login</button>
+        </div>
+    </form>
+</body>
+</html>
